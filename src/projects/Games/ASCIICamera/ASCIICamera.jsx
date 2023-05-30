b1ef23a768e284501926dfbe7c22895cb5263f59
Bazargaziyev Nursultan <nursultanbazargaziyev@gmail.com>
2023-05-30 08:57:30 +0200

    Finished ASCII Camera

--- src/projects/Games/ASCIICamera/ASCIICamera.jsx
+++ src/projects/Games/ASCIICamera/ASCIICamera.jsx
@@ -1,73 +1,118 @@
 import React, { useEffect } from "react";
-import image from "img/dog3.png";
 
 const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
-
+// $@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.
 const ASCIICamera = () => {
-  useEffect(() => {
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    const fontSize = "16px";
+  let stream = null;
+  let record = null;
+  let ctx;
+  let width = 120;
+  let height = 72;
+  const fontsize = window.innerWidth / width;
+  console.log(fontsize);
 
-    const canvas = document.getElementById("asciicam");
-    const ctx = canvas.getContext("2d");
+  const init = async () => {
+    record = document.getElementById("recorder");
 
-    const stream = document.getElementById("stream");
-    const ctxStream = stream.getContext("2d");
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      record.srcObject = stream;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    let img = new Image();
-    img.src = image;
-    let imgData;
+  const draw = () => {
+    ctx.drawImage(record, 0, 0, width, height);
+    const imgData = ctx.getImageData(0, 0, width, height);
+    const len = density.length;
+    const ratio = 255 / len;
 
-    canvas.width = width;
-    canvas.height = height;
+    const parent = document.getElementById("parent");
+    let row = "";
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        let position = (width * i + j) * 4;
 
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalWidth);
-      imgData = ctx.getImageData(0, 0, width, height);
+        let r = imgData.data[position];
+        let g = imgData.data[position + 1];
+        let b = imgData.data[position + 2];
 
-      let w = width / img.naturalWidth;
-      let h = height / img.naturalHeight;
-      stream.width *= w;
-      stream.height *= h;
-
-      console.log(w, h);
-
-      for (let i = 0; i < img.naturalWidth; i++) {
-        for (let j = 0; j < img.naturalWidth; j++) {
-          let position = (width * j + i) * 4;
-
-          let r = imgData.data[position];
-          let g = imgData.data[position + 1];
-          let b = imgData.data[position + 2];
-
-          let avg = (r + g + b) / 3;
-
-          ctxStream.fillStyle = `rgba(${avg}, ${avg}, ${avg}, 1)`;
-
-          ctxStream.fillRect(i * w, j * h, w, w);
+        let avg = (r + g + b) / 3;
+        let index = Math.floor(avg / ratio);
+        let c = density.charAt(len - 1 - index);
+        if (c === " ") {
+          row += "&nbsp;";
+        } else {
+          row += c;
         }
       }
+      row += "<br/>";
+    }
+    parent.innerHTML = row;
+    requestAnimationFrame(draw);
+  };
 
-      console.log("end");
-    };
+  useEffect(() => {
+    const canvas = document.getElementById("asciicam");
+    ctx = canvas.getContext("2d");
+    canvas.width = width;
+    canvas.height = height;
+    init();
+    draw();
   }, []);
 
   return (
-    <>
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        background: "#000",
+        justifyContent: "center",
+      }}
+    >
       <canvas
-        style={{ position: "absolute", visibility: "hidden", zIndex: "-1" }}
+        style={{
+          position: "absolute",
+          top: "0",
+          right: "0",
+          visibility: "hidden",
+          zIndex: "-1",
+        }}
         id="asciicam"
       ></canvas>
-      <canvas style={{}} id="stream"></canvas>
-    </>
+      <video
+        style={{
+          visibility: "hidden",
+          position: "absolute",
+          width: `${width}px`,
+          height: `${height}px`,
+          top: "0",
+          right: "0",
+        }}
+        src=""
+        id="recorder"
+        playsInline
+        autoPlay
+        muted
+      ></video>
+      <div
+        id="parent"
+        style={{
+          maxWidth: "100%",
+          height: "100%",
+          color: "#fff",
+          fontFamily: "Courier",
+          fontSize: "14pt",
+          lineHeight: "10pt",
+          color: "#0f0",
+        }}
+      ></div>
+    </div>
   );
 };
 
 export default ASCIICamera;
-
-// window.onresize = () => {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-//   ctx.drawImage(img, 0, 0, window.innerWidth, window.innerHeight);
-// };
